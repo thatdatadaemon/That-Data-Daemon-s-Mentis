e319f9f49afccd18a961d52442e5490460697915
thatdatadaemon <tm.valaront@gmail.com>
2024-06-28 12:20:12 +0000

    After deployment

--- app/frontend/src/pages/ask/Ask.tsx
+++ app/frontend/src/pages/ask/Ask.tsx
@@ -231,7 +231,7 @@ export function Component(): JSX.Element {
                 <h1 className={styles.askTitle}>Ask your data</h1>
                 <div className={styles.askQuestionInput}>
                     <QuestionInput
-                        placeholder="Example: Does my plan cover annual eye exams?"
+                        placeholder="Example: Talk to me about the suez canal disruption"
                         disabled={isLoading}
                         initQuestion={question}
                         onSend={question => makeApiRequest(question)}
