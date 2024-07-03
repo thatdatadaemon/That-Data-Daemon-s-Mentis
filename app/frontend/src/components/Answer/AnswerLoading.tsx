a2778ad15ecb4d62778e0c69f6f0499cef9fae57
thatdatadaemon <tm.valaront@gmail.com>
2024-07-03 15:03:12 +0000

    Changed Answerloading to Mentis Thinking

--- app/frontend/src/components/Answer/AnswerLoading.tsx
+++ app/frontend/src/components/Answer/AnswerLoading.tsx
@@ -16,7 +16,7 @@ export const AnswerLoading = () => {
                 <AnswerIcon />
                 <Stack.Item grow>
                     <p className={styles.answerText}>
-                        Generating answer
+                        Mentis Thinking
                         <span className={styles.loadingdots} />
                     </p>
                 </Stack.Item>
